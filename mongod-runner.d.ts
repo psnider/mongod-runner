3ea5f628c3563126e9a5e8871e52f9efe3ea377e
Peter Snider <snider.peter@gmail.com>
2016-10-09 10:49:55 -0700

    added Options type

--- mongod-runner.d.ts
+++ mongod-runner.d.ts
@@ -1,14 +1,22 @@
+// - port: The port to use for this mongod instance.
+//     Defaults to mongodb default port of 27017.
+// - use_tmp_dir: Set this to true to create a temporary directory that will be deleted when the calling process exits.
+// - db_path: The path to the data storage directory for this mongod instance.
+//     Note that this is not used if *use_tmp_dir* is true.
+// - disable_logging: Set this to true if you want to disable logging from mongod-runner.
+// - log_path: The path to the logging directory for this mongod instance.
+//     If this is not set, then logging is disabled for mongod.
+export interface Options {
+    port?: number | string
+    use_tmp_dir?: boolean
+    db_path?: string
+    disable_logging?: boolean
+    log_path?: string
+}
+
+
 export class MongoDaemon {
-    // @param options
-    //     - port: The port to use for this mongod instance.
-    //       Defaults to mongodb default port of 27017.
-    //     - use_tmp_dir: Set this to true to create a temporary directory that will be deleted when the calling process exits.
-    //     - db_path: The path to the data storage directory for this mongod instance.
-    //       Note that this is not used if *use_tmp_dir* is true.
-    //     - disable_logging: Set this to true if you want to disable logging from mongod-runner.
-    //     - log_path: The path to the logging directory for this mongod instance.
-    //       If this is not set, then logging is disabled for mongod.
-    constructor(options: {port?: number | string, use_tmp_dir?: boolean, db_path?: string, disable_logging?: boolean, log_path?: string})
+    constructor(options: Options)
     // Start up a mongod instance, and report completion via a callback.
     // This call is for use with mocha test before().
     start(done: (error?: Error) => void): void 
